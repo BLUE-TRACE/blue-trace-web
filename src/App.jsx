065bb1195b87464681c1bc189425a2ecc152f145
Hashani-Herath <nadeesha01ngc@gmail.com>
2026-04-08 23:55:39 +0530

    Integrate Redux store into App component and fetch users on mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import router from "./routes/routes.jsx";
+import { fetchUsers } from "./store/thunks/userThunks";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return <RouterProvider router={router} />;
 }
 
